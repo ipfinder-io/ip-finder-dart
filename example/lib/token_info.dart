c7a00f8b93067359f3b06e4ba2a534afcb8bd270
Mohamed Ben <mohamed@ipfinder.io>
2019-09-20 21:27:45 +0100

    add type annotation

--- example/lib/token_info.dart
+++ example/lib/token_info.dart
@@ -2,6 +2,6 @@ import 'package:ipfinder/ipfinder.dart';
 
 void main() async {
   Ipfinder ipfinder = Ipfinder("YOUR_TOKEN_GOES_HERE");
-  var status = await ipfinder.getStatus();
+  StatusResponse status = await ipfinder.getStatus();
   print(status.toJson()); // print json data
 }
